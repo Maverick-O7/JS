f026a234991ff98c5fdaf47c819c894ba04f20b3
Maverick-O7 <72644870+Maverick-O7@users.noreply.github.com>
2025-10-17 10:12:17 +0000

    Pushed dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use scrict" //treat al JS code as newer version
+
+//alert (3+3) //we are using nodeJS, not browser
+
+console.log(3
+    +3); //Code readability should be high
+
+let name = "abc"
+let age = 25
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof "abc");
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
